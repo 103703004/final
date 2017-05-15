c50cdf69ba1be8d738d6759dcefd2fc5df877312
Wan-Ting Huang <103703004@nccu.edu.tw>
2017-05-15 20:05:19 +0800

    navbar、圖

--- shop.js
+++ shop.js
@@ -1,3 +1,14 @@
-$(document).ready(function(){
-    $("ul").css("display","none");
+$("#menuIcon").on("click",function(){
+    //$("ul").css("display","none");
+    $("ul").toggle();
+    $(".foodNav").hide();
+    $(".shopNav").hide();
+});
+
+$("#firstNav").on("click",function(){
+    $(".foodNav").toggle();
+});
+
+$("#secNav").on("click",function(){
+    $(".shopNav").toggle();
 });
