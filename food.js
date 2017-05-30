fdbc66c04dd6dac2ea97d16b74a5c9508387cfd5
Wan-Ting Huang <103703004@nccu.edu.tw>
2017-05-30 13:10:32 +0800

    pictures

---
+++ food.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAZexUoWbiC2J7RJf9qGAhvxkEnqDqqzfk",
+    authDomain: "webfinal-e58b9.firebaseapp.com",
+    databaseURL: "https://webfinal-e58b9.firebaseio.com",
+    projectId: "webfinal-e58b9",
+    storageBucket: "webfinal-e58b9.appspot.com",
+    messagingSenderId: "198736994181"
+  };
+  firebase.initializeApp(config);
